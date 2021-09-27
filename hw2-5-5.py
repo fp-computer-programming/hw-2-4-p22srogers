e61870af38a465587fa9ce98200bddcb923d66e4
p22srogers <90051572+p22srogers@users.noreply.github.com>
2021-09-27 10:04:36 -0400

    Update hw2-5-5.py

--- hw2-5-5.py
+++ hw2-5-5.py
@@ -3,6 +3,6 @@ a=1
 b=3
 c=-4
 square_root=b**2-4*(a)*(c)
-first_solution= ((-b)+(square_root**2))/((2)*(a))
-second_solution= ((-b)-(square_root**2))/((2)*(a))
+first_solution= ((-b)+(square_root**1/2))/((2)*(a))
+second_solution= ((-b)-(square_root**1/2))/((2)*(a))
 print(first_solution,second_solution)
